e2b9843b317bde3bb185dbb4a28f10ff0c6d0d21
Navishkar Rao <navrao@live.com>
2013-02-12 10:50:23 +1300

    add browser conditionals for compatitibility with IE

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,4 +1,8 @@
 <!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>" class="no-js">
 <head>
   <title><?php print $head_title; ?></title>
